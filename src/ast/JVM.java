e436b0835118cc3dc491c3c50df33afdceba8647
Anton Kochergin <87330809+sora-vwv@users.noreply.github.com>
2023-09-06 22:13:35 +0400

    Update JVM.java

--- src/ast/JVM.java
+++ src/ast/JVM.java
@@ -4,55 +4,15 @@ import ast.compatibility.IClass;
 import ast.compatibility.IList;
 
 import java.util.Objects;
-
-/*
-В Java Virtual Machine (JVM) типы данных записываются иными способами. Для этого
-необходимо создать класс, который будет конвертировать привычные нам типы данных
-в типы данных JVM. Обозначения:
-
-void           V      это не тип данных, но тоже есть
-boolean        Z
-char           C
-byte           B
-short          S
-int            I
-long           J
-float          F
-double         D
-
-В массивах сначала идут уровень/мерность массива, а после тип который он хранит:
-
-byte[][][]     [[[B
-double[][]     [[D
-boolean[]      [Z
-
-Объекты же здесь зовутся Reference и имена здесь указываются полными:
-
-Object         java/lang/Object
-String         java/lang/String
-ArrayList      java/util/ArrayList
-
-Но если использовать reference как тип данных, то его необходимо обарачивать в L;
-
-Object         Ljava/lang/Object;
-String         Ljava/lang/String;
-ArrayList      Ljava/util/ArrayList;
-
-Массивы могут восприниматься как reference, но не имеют ту же обёртку.
-
-*/
+
 public class JVM {
-    // хранение в виде JVM
+
     private final String value;
 
-    // если это ARRAY, то храним мерность
     private final int depth;
 
-    // тип данных или тип массива
     private final Type type;
 
-    // храним тип данных как константу enum
-    // для более наглядной работы с типами JVM
     public enum Type {
         VOID,       // V
         BOOLEAN,    // Z
@@ -65,8 +25,6 @@ public class JVM {
         DOUBLE,     // D
         REFERENCE   // L...;
     }
-
-    // всякие нужные в будующем конструкторы
 
     public JVM(Type type) throws AstException {
         this.value = switch (type) {
@@ -121,11 +79,7 @@ public class JVM {
         this.type = parent.type;
         this.depth = depth;
     }
-
-    // is и get
-
-    // JVM в операциях не разделяет типы данных byte, short и int. Для неё всё - int.
-    // Разделение существует во всех остальных случаях (сигнатура методов и т.д.)
+
     public boolean isIntJVM() {
         return type == Type.CHAR || type == Type.BYTE || type == Type.SHORT || type == Type.INT;
     }
@@ -166,13 +120,10 @@ public class JVM {
         return type == Type.DOUBLE && !isArray();
     }
 
-    // потому что отрицание часто используется
     public boolean isNotNumber() {
         return !isIntJVM() && !isLong() && !isFloat() && !isDouble();
     }
 
-    // хоть для JVM массивы и reference могут быть одним и тем-же
-    // в Abstract Syntax Tree необходимо четко разделять их
     public boolean isReference() {
         return type == (Type.REFERENCE) && !isArray();
     }
@@ -181,7 +132,6 @@ public class JVM {
         return depth != 0;
     }
 
-    // отдельно для массивов
     public boolean isArrayIntJVM() {
         return isArray() && (type == Type.CHAR || type == Type.BYTE || type == Type.SHORT || type == Type.INT);
     }
@@ -226,7 +176,6 @@ public class JVM {
         return depth;
     }
 
-    // тип аргумента метода и т. д., где надо L;
     public String getInternalType() {
         if (isReference())
             return 'L' + value + ';';
@@ -252,7 +201,6 @@ public class JVM {
         return value;
     }
 
-    // можно записать короче, но так понятнее.
     public boolean equals(JVM o) throws AstException {
         if (depth != o.depth)
             return false;
@@ -279,7 +227,6 @@ public class JVM {
         return Objects.equals(value, o.value);
     }
 
-    // чем выше scale, тем хуже сравнение. До получения scale необходимо сравнить с помощью equals
     public int getScale(JVM o) throws AstException {
 
         if (Objects.equals(value, o.value))
